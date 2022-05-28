de3bb2c0a3a2a6ccf804c43b6a3a0371c4d7ef8f
marco-silveira <marcosilveira.sa@gmail.com>
2022-05-28 15:25:36 -0300

    ajustando-sql-init

--- config/db/quickeat_database.sql
+++ config/db/quickeat_database.sql
@@ -1,15 +1,9 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.5.8-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             11.0.0.5919
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=1 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
 SET foreign_key_checks = 1;
 
 -- Dumping database structure for quickeat
@@ -20,7 +14,12 @@ USE `quickeat`;
 CREATE TABLE IF NOT EXISTS `categorias` (
     `id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `categorias` (`id`) VALUES
+    (1),
+    (2),
+    (3);
 
 -- Dumping data for table quickeat.categorias: ~0 rows (approximately)
 /*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
@@ -35,17 +34,17 @@ CREATE TABLE IF NOT EXISTS `pratos` (
     `tempo_de_preparo` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'MINUTOS',
     PRIMARY KEY (`id`),
     KEY `id_categoria` (`id_categoria`),
-    CONSTRAINT `id_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+    CONSTRAINT `pratos_categoria_fk` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Dumping data for table quickeat.pratos: ~0 rows (approximately)
 /*!40000 ALTER TABLE `pratos` DISABLE KEYS */;
 INSERT INTO `pratos` (`id`, `id_categoria`, `nome`, `preco`, `tempo_de_preparo`) VALUES
-    (1, NULL, 'tilápia', 45.00, 30),
-    (2, NULL, 'batata frita', 15.00, 15),
-    (3, NULL, 'batata frita com queijo', 18.00, 16),
-    (4, NULL, 'iscas de frango acebolada', 20.00, 20),
-    (5, NULL, 'filé parmegiana', 40.00, 40);
+   (1, NULL, 'tilápia', 45.00, 30),
+   (2, NULL, 'batata frita', 15.00, 15),
+   (3, NULL, 'batata frita com queijo', 18.00, 16),
+   (4, NULL, 'iscas de frango acebolada', 20.00, 20),
+   (5, NULL, 'filé parmegiana', 40.00, 40);
 /*!40000 ALTER TABLE `pratos` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
